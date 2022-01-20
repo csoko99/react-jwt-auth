511b0391aaea73b3d3fd048a3a763c5e49a5f566
tanulo <tanulo@barossgabor.local>
2022-01-20 12:12:56 +0100

    l

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Profile from "./components/profile.component";
 import BoardUser from "./components/board-user.component";
 import BoardModerator from "./components/board-moderator.component";
 import BoardAdmin from "./components/board-admin.component";
-import Proba from "./components/sajatosztajok/Proba"
+import Proba from "./sajatosztajok/Proba"
 
 class App extends Component {
   constructor(props) {
